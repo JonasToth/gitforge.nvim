dc844c2c94cb068fceb785379630a6591792470e
Jonas Toth <development@jonas-toth.eu>
2025-01-17 15:41:28 +0100

    refactor: use sets in generic-issue api for labels and assignees

--- lua/gitforge/generic_issue.lua
+++ lua/gitforge/generic_issue.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+require("gitforge.set")
+
 ---Author of an issue or comment, corresponding to a github user or bot.
 ---@class Author
 ---@field login string Username of the author.
@@ -120,7 +122,7 @@ end
 --- Parses the buffer name and tries to retrieve the issue number and project.
 ---@param buf number Buffer Id for the issue buffer
 function M.get_issue_id_from_buf(buf)
-    local buf_name = a.nvim_buf_get_name(buf)
+    local buf_name = vim.api.nvim_buf_get_name(buf)
     if string.find(buf_name, "[Issue]", 1, true) == nil then
         return nil
     end
@@ -132,10 +134,10 @@ end
 
 ---Return the comma separated list of labels from the issue buffer @c buf if possible.
 ---@param buf number Buffer-ID for issue.
----@return string|nil labels a comma separated list of labels on success, otherwise @c nil.
+---@return Set|nil labels Set of extracted labels. @c nil if that failed.
 function M.get_labels_from_issue_buffer(buf)
     local log = require("gitforge.log")
-    local curr_buf_content = a.nvim_buf_get_lines(buf, 6, 7, false)
+    local curr_buf_content = vim.api.nvim_buf_get_lines(buf, 6, 7, false)
     local label_line = curr_buf_content[1]
     if label_line == nil then
         log.notify_failure("Failed to get expected label line from buffer " .. buf)
@@ -143,11 +145,55 @@ function M.get_labels_from_issue_buffer(buf)
     end
     -- verify that the labels line is found
     if label_line:sub(1, 7) ~= "Labels:" then
-        return ""
+        return Set:new()
     end
     -- extract all labels from the line
-    return label_line:sub(9, -1)
+    return Set:createFromCSVList(label_line:sub(9, -1))
 end
 
+---Return the string of the current state of the issue buffer @c buf if possible.
+---@param buf number Buffer-Id for the issue
+---@return string|nil status String representation of the issue status if found,  otherwise @c nil.
+function M.get_status_from_issue_buffer(buf)
+    local log = require("gitforge.log")
+    local curr_buf_content = vim.api.nvim_buf_get_lines(buf, 4, 5, false)
+    local status_line = curr_buf_content[1]
+    if status_line == nil then
+        log.notify_failure("Failed to get status line from buffer " .. buf)
+        return nil;
+    end
+    local extracted_status = string.match(status_line, "^Status: (.+) %(")
+    if extracted_status == nil then
+        log.notify_failure("Failed to extract status from buffer " .. buf .. " and supposed state line:\n" .. status_line)
+        return nil
+    end
+    return vim.trim(extracted_status)
+end
+
+---Return the comma separated list of assignees from the issue buffer @c buf if possible.
+---@param buf number Buffer-ID for Issue.
+---@return Set|nil assignees A set of assignees or @c nil if extraction failed.
+function M.get_assignee_from_issue_buffer(buf)
+    local log = require("gitforge.log")
+    local curr_buf_content = vim.api.nvim_buf_get_lines(buf, 5, 6, false)
+    local assignee_line = curr_buf_content[1]
+    if assignee_line == nil then
+        log.notify_failure("Failed to get assignee line from buffer " .. buf)
+        return nil;
+    end
+    -- verify that the labels line is found
+    if assignee_line:sub(1, 12) ~= "Assigned to:" then
+        log.notify_failure("Found assignee line does not contain 'Assigned to:' at beginning of line, ERROR (line: " ..
+            assignee_line .. ")")
+        return nil
+    end
+    -- extract all labels from the line
+    local assignees = assignee_line:sub(14, -1)
+    if assignees == "-" then
+        return Set:new()
+    else
+        return Set:createFromCSVList(assignees)
+    end
+end
 
 return M
